fde179b9fa6b7d21dd947963a32e732d6527d083
Peter Lewandowski <mapiotr2010@gmail.com>
2023-01-07 22:00:49 -0500

    getting the article data via getServerSideProps example

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,14 +1,33 @@
 //single article page
 
-import { useRouter } from "next/router"
 
-const article = () => {
-    const router = useRouter()
-    const {id} = router.query
+import Link from "next/link";
+import { useRouter } from "next/router";
 
-  return (
-    <div>This is article {id}</div>
-  )
-}
+const article = ({ article }) => {
+  // const router = useRouter()
+  // const {id} = router.query
 
-export default article
+  return <>
+    <h1>{article.title}</h1>
+    <p>{article.body}</p>
+    <br/>
+    <Link href='/'>Go Back</Link>
+  </>
+};
+
+export const getServerSideProps = async (context) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  );
+
+  const article = await res.json();
+
+  return {
+    props: {
+      article,
+    },
+  };
+};
+
+export default article;
