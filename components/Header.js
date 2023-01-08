5ed2b8cf1508bf9f99d38b10523fbfca1977258e
Peter Lewandowski <mapiotr2010@gmail.com>
2023-01-07 19:28:47 -0500

    basic header

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,13 @@
-import headerstyles from "../styles/Header.module.css";
+import headerStyles from "../styles/Header.module.css";
 
 const Header = () => {
-  const x = 2;
+
   return (
     <div>
-      <h1 className="title">
+      <h1 className={headerStyles.title}>
         <span>WebDev</span>News
       </h1>
-      <style jsx>
-        {`
-          .title {
-            color: ${x > 3 ? "red" : "blue"};
-          }
-        `}
-      </style>
+      <p className={headerStyles.description}>Keep up to date with the latest web dev news</p>
     </div>
   );
 };
