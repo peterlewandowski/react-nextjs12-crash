74d1b6c2d99012571830baf87b0ac515a26af30c
Peter Lewandowski <mapiotr2010@gmail.com>
2023-01-08 09:31:30 -0500

    add Meta component

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,12 @@
 import Header from './Header';
 import Nav from './Nav'
+import Meta from './Meta';
 import styles from "../styles/Layout.module.css";
 
 const Layout = ({ children }) => {
   return (
     <>
+    <Meta />
     <Nav />
     <div className={styles.container}>
       <main className={styles.main}>
