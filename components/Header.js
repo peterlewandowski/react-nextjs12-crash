7d8172dec137b4bf94673b5ab87ae3917d9282da
Peter Lewandowski <mapiotr2010@gmail.com>
2023-01-07 19:23:45 -0500

    inline conditional style rendering example

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,21 @@
-import headerstyles from '../styles/Header.module.css'
+import headerstyles from "../styles/Header.module.css";
 
 const Header = () => {
+  const x = 2;
   return (
     <div>
-        <h1 className='title'>
-            <span>WebDev</span>News
-        </h1>
-        <style jsx>
-            {`
-            .title {
-                color: red}`}
-        </style>
+      <h1 className="title">
+        <span>WebDev</span>News
+      </h1>
+      <style jsx>
+        {`
+          .title {
+            color: ${x > 3 ? "red" : "blue"};
+          }
+        `}
+      </style>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
