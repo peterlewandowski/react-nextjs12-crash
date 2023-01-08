0de3d9dd82aee756096fc8b4c88e5d2dfb89be73
Peter Lewandowski <mapiotr2010@gmail.com>
2023-01-07 20:13:52 -0500

    quick cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,28 @@
 import Head from "next/head";
+import ArticleList from "../components/ArticleList";
 
-export default function Home({articles}) {
-  console.log({articles})
-  
+export default function Home({ articles }) {
   return (
     <div>
       <Head>
         <title>WebDev News</title>
-        <meta name='keywords' content='web development, programming'></meta>
+        <meta name="keywords" content="web development, programming"></meta>
       </Head>
-      <h1>Welcome to Next</h1>
+
+      <ArticleList articles={articles} />
     </div>
   );
 }
 
 export const getStaticProps = async () => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
-  const articles = await res.json()
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=6`
+  );
+  const articles = await res.json();
 
   return {
     props: {
-      articles
-    }
-  }
-}
+      articles,
+    },
+  };
+};
